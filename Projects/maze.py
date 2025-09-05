5caf3b109801aa37d5660dcd117de1975a25b926
Vaidul1S <vaidas.ad25@gmail.com>
2025-09-05 12:27:50 +0300

    09.05

--- Projects/maze.py
+++ Projects/maze.py
@@ -32,6 +32,21 @@ def print_maze(maze, stdscr, path=[]):
         for j, value in enumerate(row):
             stdscr.addstr(i, j*2, value, GREEN)
 
+def find_start(maze, start):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return i, j
+
+def find_path(maze, stdscr):
+    start = "O"
+    end = "X"
+    start_pos = find_start(maze, start)
+
+    q = queue.Queue()
+    
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
