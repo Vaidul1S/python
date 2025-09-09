dc177af37bae0963b7a2d0ff8eab779c08131ee4
Vaidul1S <vaidas.ad25@gmail.com>
2025-09-09 11:31:47 +0300

    09.09

--- Projects/youtube_downloader.py
+++ Projects/youtube_downloader.py
@@ -6,7 +6,7 @@ def download_video(url, save_path):
     try:
         yt = YouTube(url)
         streams = yt.streams.filter(progressive=True, file_extension="mp4")
-        highest_res_stream = streams.get_highest_resolution()
+        highest_res_stream = streams.get_by_resolution("720p")
         highest_res_stream.download(output_path=save_path)
         print("Video downloaded successfully!")
 
