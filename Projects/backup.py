ed2e55f2f2fa183aa06927e97e40c04b87d1e622
Vaidul1S <vaidas.ad25@gmail.com>
2025-09-10 11:30:36 +0300

    09.10

--- Projects/backup.py
+++ Projects/backup.py
@@ -6,3 +6,15 @@ import time
 
 source_dir = "C:/Users/vaida/Pictures/Screenshots"
 destination_dir = "C:/Users/vaida/Pictures/backups"
+
+def copy_folder_to_direcion(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in: {dest}")
+
+copy_folder_to_direcion(source_dir, destination_dir)
