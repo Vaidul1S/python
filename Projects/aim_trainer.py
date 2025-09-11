c5f5dacf0a04481460c9f39c2f7d883a6204a499
Vaidul1S <vaidas.ad25@gmail.com>
2025-09-11 12:12:49 +0300

    09.11

--- Projects/aim_trainer.py
+++ Projects/aim_trainer.py
@@ -78,6 +78,21 @@ def draw_top_bar(win, elapsed_time, targets_pressed, misses):
     win.blit(hits_label, (400, 5))
     win.blit(lives_label, (600, 5))
 
+def end_game(win, elapsed_time, targets_pressed, clicks):
+    win.blit(BG_COLOR)
+    time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, "black")
+
+    speed = round(targets_pressed / elapsed_time, 1)
+    speed_label = LABEL_FONT.render(f"Speed: {speed} t/s", 1, "black")
+    hits_label = LABEL_FONT.render(f"Hits: {targets_pressed}", 1, "black")
+    accuracy = round(targets_pressed / clicks * 100, 1)
+    accuracy_label = LABEL_FONT.render(f"Accuracy: {accuracy}", 1, "black")
+
+    win.blit(time_label, (5, 5))
+    win.blit(speed_label, (200, 5))
+    win.blit(hits_label, (400, 5))
+    win.blit(accuracy_label, (600, 5))
+
 def main():
     run = True
     targets = []
