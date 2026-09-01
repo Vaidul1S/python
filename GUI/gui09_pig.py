c0e5bdb493953cee48dcb6775e0a46df867747a5
Vaidul1S <vaidas.ad25@gmail.com>
2026-09-01 10:57:29 +0300

    09.01

---
+++ GUI/gui09_pig.py
@@ -0,0 +1,63 @@
+import random
+import sys
+import time
+import math
+import pygame
+pygame.init()
+# Pig is a simple dice game first described in print by John Scarne in 1945. 
+# Players take turns to roll a single dice as many times as they wish, adding all roll results to a running total, 
+# but losing their gained score for the turn if they roll a 1.
+
+WIDTH, HEIGHT = 1000, 800
+
+def roll():
+    min = 1
+    max = 6
+    roll = random.randint(min, max)
+    return roll
+
+while True: 
+    players = input("Enter the number of players(2-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <=4:
+            break
+        else:
+            print("Number of players must be beeen 2 - 4!")
+    else:
+        print("Invalid number of players!")
+
+# winning condition     
+max_score = 50
+players_scores = [0 for _ in range(players)]
+
+while max(players_scores) < max_score:
+
+    for player_i in range(players):
+        print("\nPlayer", player_i + 1, "turn to roll!")
+        print("Your total score is:", players_scores[player_i], "\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll?(Y/N): ").lower()
+            if should_roll != "y":
+                break
+
+            value = roll()
+            if value == 1:
+                print("You rolled 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled a:", value)
+            
+            print("Your score is", current_score)
+
+        players_scores[player_i] += current_score
+        print("Your total score is:", players_scores[player_i])
+
+max_score = max(players_scores)
+winner_i = players_scores.index(max_score)
+print("Player number", winner_i + 1, "is the winner with a score of:", max_score)
+
