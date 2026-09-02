1b6883e9019b311a7f30bd7e159e9675b413fbe1
Vaidul1S <vaidas.ad25@gmail.com>
2026-09-02 19:52:30 +0300

    09.02

--- GUI/gui09_pig.py
+++ GUI/gui09_pig.py
@@ -4,6 +4,7 @@ import time
 import math
 import pygame
 pygame.init()
+
 # Pig is a simple dice game first described in print by John Scarne in 1945. 
 # Players take turns to roll a single dice as many times as they wish, adding all roll results to a running total, 
 # but losing their gained score for the turn if they roll a 1.
