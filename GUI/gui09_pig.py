8c5871c1ec0b9b9a9a491ae2a15ee70a811739ac
Vaidul1S <vaidas.ad25@gmail.com>
2026-09-07 10:53:31 +0300

    09.07

--- GUI/gui09_pig.py
+++ GUI/gui09_pig.py
@@ -64,12 +64,12 @@ button_color = "green"
 
 dice_art = {
     0: ("┌─────┐\n|                       |\n|                       |\n|                       |\n└─────┘"),
-    1: ("┌─────┐\n|                      |\n|         ♣         |\n|                      |\n└─────┘"),
-    2: ("┌─────┐\n|     ♣             |\n|                      |\n|             ♣     |\n└─────┘"),
-    3: ("┌─────┐\n|     ♣             |\n|         ♣         |\n|             ♣     |\n└─────┘"),
-    4: ("┌─────┐\n|    ♣       ♣    |\n|                       |\n|    ♣       ♣    |\n└─────┘"),
-    5: ("┌─────┐\n|    ♣       ♣    |\n|         ♣         |\n|    ♣       ♣    |\n└─────┘"),
-    6: ("┌─────┐\n|    ♣       ♣    |\n|    ♣       ♣    |\n|    ♣       ♣    |\n└─────┘"),
+    1: ("┌─────┐\n|                       |\n|          1          |\n|                       |\n└─────┘"),
+    2: ("┌─────┐\n|     2                |\n|                       |\n|                2     |\n└─────┘"),
+    3: ("┌─────┐\n|     3                |\n|          3          |\n|                3     |\n└─────┘"),
+    4: ("┌─────┐\n|     4         4     |\n|                       |\n|     4         4     |\n└─────┘"),
+    5: ("┌─────┐\n|     5         5     |\n|          5          |\n|     5         5     |\n└─────┘"),
+    6: ("┌─────┐\n|     6         6     |\n|     6         6     |\n|     6         6     |\n└─────┘"),
 }
 
 class MainWindow(QMainWindow):
@@ -115,8 +115,8 @@ class MainWindow(QMainWindow):
 
         self.dice_button = QPushButton(f"{dice_art[0]}")
         self.dice_button.setFixedWidth(180)      
-        self.dice_button.setFont(QFont("times", 18))
-        self.dice_button.setStyleSheet(f"background-color: {button_color};" f"color: {font_color};" "border-radius: 10px;" "padding: 5px;")
+        self.dice_button.setFont(QFont("times", 17))
+        self.dice_button.setStyleSheet(f"background-color: {button_color};" f"color: {font_color};" "border-radius: 10px;" "padding: 5px;" "font-weight: bold;")
         self.main_layout.addWidget(self.dice_button)
 
         self.points_box = QHBoxLayout()
